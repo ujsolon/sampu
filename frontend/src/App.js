d02cfd2984e4369bc551c2136a172a6fdffa7c25
ujsolon <ujsolon@gmail.com>
2024-07-26 14:54:50 +0800

    create gamescreen page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import GamesScreen from './screens/GamesScreen';
 
 function App() {
     return (
-        <div className="App">
-            <HomeScreen />
-        </div>
+        <Router>
+            <div className="App">
+                <header>
+                    <h1>Sampu</h1>
+                    <nav>
+                        <Link to="/">Courts</Link>
+                        <Link to="/games">Games</Link>
+                        <Link to="/account">My account</Link>
+                    </nav>
+                </header>
+
+                <Routes>
+                    <Route path="/" element={<HomeScreen />} />
+                    <Route path="/games" element={<GamesScreen />} />
+                    {/* Add more routes as needed */}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
