9b48c73fd93a2a7ed945c14ae66c5583367bede5
ujsolon <ujsolon@gmail.com>
2024-07-29 14:23:43 +0800

    Test env var in vercel

--- frontend/src/utils/supabase.js
+++ frontend/src/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 console.log('Supabase URL:', supabaseUrl)
 console.log('Supabase Anon Key:', supabaseAnonKey)
